28f8c127e94a31340e7aebc9504e15d2842bc006
Tibor Kircsi <tkircsi@gmail.com>
2020-01-07 22:54:10 +0100

    test Azure DevOps CI CD

--- sendMessage/index.js
+++ sendMessage/index.js
@@ -1,10 +1,11 @@
 ﻿const df = require("durable-functions");
 
 module.exports = async function (context, req) {
-    const client = df.getClient(context);
-    const instanceId = await client.startNew(req.params.functionName, undefined, req.body);
+  const client = df.getClient(context);
+  const instanceId = await client.startNew(req.params.functionName, undefined, req.body);
 
-    context.log(`Started orchestration with ID = '${instanceId}'.`);
+  context.log('*** Function started ****');
+  context.log(`Started orchestration with ID = '${instanceId}'.`);
 
-    return client.createCheckStatusResponse(context.bindingData.req, instanceId);
+  return client.createCheckStatusResponse(context.bindingData.req, instanceId);
 };
