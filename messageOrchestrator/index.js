e7aee0f2a7cd78d978b5efc269f6c26a2ab9a730
Kircsi Tibor <kirtib@fundamenta.local>
2020-01-07 21:50:37 +0100

    initial commit

---
+++ messageOrchestrator/index.js
@@ -0,0 +1,24 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+    const input = context.df.getInput();
+    input['filename'] = `file_${input['name']}.txt`;
+
+    outputs.push(yield context.df.callActivity("logMessage", input));
+    outputs.push(yield context.df.callActivity("sendEmail", input.email));
+
+
+    return outputs;
+});
---
+++ sendEmail/index.js
@@ -0,0 +1,26 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env['SENDGRID_API_KEY']);
+
+module.exports = async function (context) {
+  const email = context.bindings.email;
+  const msg = {
+    to: email,
+    from: { email: 'tibcsi@azure.dev', name: 'Tibor Kircsi(Azure)' },
+    subject: `Fundamenta teszt üzenet.`,
+    html: `<h4>Tisztelt Ügyfelünk!</h4> <p>Ez már valami!</p>`
+  };
+sgMail.send(msg);
+
+    return msg;
+};
