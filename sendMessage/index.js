e78b4c49ee396c39679827ed856f100c9d0b6158
Tibor Kircsi <tkircsi@gmail.com>
2020-01-07 23:35:10 +0100

    test Azure DevOps CI CD 3

--- sendMessage/index.js
+++ sendMessage/index.js
@@ -4,6 +4,7 @@ module.exports = async function (context, req) {
   const client = df.getClient(context);
   const instanceId = await client.startNew(req.params.functionName, undefined, req.body);
 
+  context.log('*** Function started ****');
   context.log(`Started orchestration with ID = '${instanceId}'.`);
 
   return client.createCheckStatusResponse(context.bindingData.req, instanceId);
