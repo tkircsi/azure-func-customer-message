9e29e03110fb262627db6a58ea7e3965b48cf9ac
Kircsi Tibor <kirtib@fundamenta.local>
2020-01-08 15:24:53 +0100

    add sms sending activity

---
+++ sendSMS/index.js
@@ -0,0 +1,19 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+module.exports = async function (context) {
+
+  const { message, phone, name } = context.bindings.payload;
+  context.bindings.smsmessage = {
+    body: `Tisztelt ${name}! Az üzenetét megkaptuk! Üdvözlettel, Tibor`,
+    to: phone
+  };
+};
